47cfe2b072440dea470b51ccec13efeba4bbcd38
Chris Grochmal <csg6as@virginia.edu>
2021-01-20 21:40:53 -0700

    unicorn

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,6 +1,16 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color = 'white') {
+    this.name = name
+    this.color = color
+  }
+
+  isWhite() {
+    return this.color === 'white'
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
 
-module.exports = Unicorn;
+module.exports = Unicorn
